01100e761902e04111c7a526751ee21c576e597a
nicoallas <nicolasallas93@gmail.com>
2022-04-14 14:21:16 -0300

    js

---
+++ js/index.js
@@ -0,0 +1,30 @@
+/*
+    VARIABLES Y CONSTANTES EN JS
+*/
+
+var nombre;
+
+nombre = "Nicolás";
+
+let apellido = "Allas";
+
+const fechaDeNacimiento = '18/04/1993';
+
+/* 
+    TIPOS DE DATOS
+    STRING = Valores de tipo texto
+    NUMBER = Valores de tipo numerico
+    BOOLEAN = Valores de tipo verdadero/falso
+
+*/
+
+// Tipo de dato String (Van entre comillas dobles o simples)
+let name = "Nico";
+
+// Tipo de dato Number (Van sin comillas)
+let edad = 28;
+
+// Tipo de dato Boolean (valores true/false)
+let esMayorDeEdad = true;
+
+let sexo = 'M';
